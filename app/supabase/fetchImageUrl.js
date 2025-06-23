4677c80ca742b8b4dfaa3ae276c6929691816d9b
wpeckelsen <wpeckelsen@gmail.com>
2025-06-23 13:19:39 +0200

    connection fixes

--- app/supabase/fetchImageUrl.js
+++ app/supabase/fetchImageUrl.js
@@ -12,10 +12,3 @@ export async function fetchImageUrl(bucketName, filePath){
     return data.publicUrl;
     
 }
-
-
-        // https://[project_id].supabase.co/storage/v1/object/public/[bucket]/[asset-name]
-// https://mvaeqayvhighbhnkrydl.supabase.co/storage/v1/object/public/images/zArHI7A.png
-// 
-// https://mvaeqayvhighbhnkrydl.supabase.co/storage/v1/object/sign/images/d25tadtiyyw41.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvZDI1dGFkdGl5eXc0MS5wbmciLCJpYXQiOjE3MzIxMDU4MjYsImV4cCI6MTg4OTc4NTgyNn0.hy5b894Wnck_MAPMA1cy6KB_EvzHY6QguaFbmphYrCY&t=2024-11-20T12%3A30%3A27.024Z
-// https://mvaeqayvhighbhnkrydl.supabase.co/storage/v1/object/sign/images/flags/d25tadtiyyw41.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvZmxhZ3MvZDI1dGFkdGl5eXc0MS5wbmciLCJpYXQiOjE3MzIxMDkzNDcsImV4cCI6MTczMjcxNDE0N30.3oLGvuMZsmIcEgHFy6D3X4ATqNVwxC7YFPjBdK0r2S4&t=2024-11-20T13%3A29%3A07.287Z
